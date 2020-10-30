4d9a86e94357469b0932854a23d6ffb384c9941c
Schaeferjo <mail@joschaefer.com>
2020-10-30 16:51:43 +0100

    feat: add icon and shadow to map

--- components/HeroSectionMap.vue
+++ components/HeroSectionMap.vue
@@ -7,7 +7,16 @@
             <l-tile-layer
               url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
             ></l-tile-layer>
-            <l-marker :lat-lng="[50.8561, 7.2776]"></l-marker>
+            <l-marker :lat-lng="[50.8561, 7.2776]">
+              <l-icon
+                icon-url="/images/mapmarker.svg"
+                :icon-size="[50, 66]"
+                :icon-anchor="[25, 66]"
+                shadow-url="/images/mapmarker_shadow.svg"
+                :shadow-size="[50, 66]"
+                :shadow-anchor="[5, 66]"
+              ></l-icon>
+            </l-marker>
           </l-map>
         </client-only>
       </v-col>
