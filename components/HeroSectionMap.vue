5cf2e8d3544e675bdf7a11f3559c6e5239f3d024
Schaeferjo <mail@joschaefer.com>
2020-10-30 15:28:31 +0100

    fix: removed mapbox-gl

--- components/HeroSectionMap.vue
+++ components/HeroSectionMap.vue
@@ -8,59 +8,6 @@
   </v-parallax>
 </template>
 
-<script>
-import mapboxgl from 'mapbox-gl'
-
-export default {
-  data() {
-    return {
-      access_token:
-        'pk.eyJ1Ijoiam9zZW4iLCJhIjoiY2llcXQ4aTd0MDEzaHZ4a21jejVrcHdyMiJ9.SPCThtA30WGuMPwFxju_8w',
-      map: {},
-      link: [
-        {
-          rel: 'stylesheet',
-          href: 'https://api.mapbox.com/mapbox-gl-js/v1.8.1/mapbox-gl.css',
-        },
-      ],
-    }
-  },
-
-  mounted() {
-    this.createMap()
-  },
-  methods: {
-    createMap() {
-      mapboxgl.accessToken = this.access_token
-      this.map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/josen/ckguz143y04u619rtnoipsogl',
-        zoom: 11,
-        center: [7.2776, 50.8561],
-        minZoom: 2,
-        maxZoom: 14,
-      })
-      this.map.addControl(new mapboxgl.NavigationControl())
-      new mapboxgl.Marker({
-        color: '#5e2368',
-      })
-        .setLngLat([7.2776, 50.8561])
-        .addTo(this.map)
-    },
-  },
-  head() {
-    return {
-      link: [
-        {
-          rel: 'stylesheet',
-          href: 'https://api.mapbox.com/mapbox-gl-js/v1.8.1/mapbox-gl.css',
-        },
-      ],
-    }
-  },
-}
-</script>
-
 <style lang="scss">
 .herosectionheight,
 #map {
--- pages/kontakt.vue
+++ pages/kontakt.vue
@@ -1,6 +1,68 @@
 <template>
   <div>
     <h1>Kontakt</h1>
-    <div id="map"></div>
+    <h2>Kontaktdaten und Terminvereinbarungen</h2>
+    <p>
+      wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf
+      fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf
+      alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf
+      wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf
+      fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf
+      alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf
+      wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf
+      fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf
+      alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf
+      wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf
+      fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf
+      alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf
+      wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf
+      fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf
+      alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf
+      wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf
+      fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf
+      alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf
+      wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf
+      fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf
+      alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf
+      wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf
+      fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf
+      alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf
+      wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf
+      fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf
+      alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf
+      wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf
+      fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf
+      alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf
+      wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf
+      fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf
+      alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf
+      wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf
+      fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf
+      alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf
+      wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf
+      fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf
+      alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf
+      wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf
+      fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf
+      alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf
+      wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf
+      fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf
+      alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf
+      wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf
+      fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf
+      alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf
+      wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf
+      fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf
+      alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf
+      wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf
+      fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf
+      alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf
+      wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf
+      fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf
+      alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf
+      wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf
+      fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf wlenfkjsaf
+      alfkjasfknasdf fnasdkfnasndf wlenfkjsaf alfkjasfknasdf fnasdkfnasndf
+    </p>
   </div>
 </template>
