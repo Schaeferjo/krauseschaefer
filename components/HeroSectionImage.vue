ba410351779893a1dce08baa36abe4cbe8ec11a7
Schaeferjo <mail@joschaefer.com>
2020-11-04 13:34:23 +0100

    feat: srcsets working correctly

--- components/HeroSectionImage.vue
+++ components/HeroSectionImage.vue
@@ -1,9 +1,17 @@
+(min-width: 1600px) 1200px, // ViewPort mindestens 1600 px, nimm Bild mit 1200px
+Breite (min-width: 1400px) 1100px, // ViewPort mindestens 1400 px, nimm Bild mit
+1100px Breite (min-width:1000px) 900px, // ViewPort mindestens 1000 px, nimm
+Bild mit 900px Breite 100vw"
+
 <template>
   <v-img
     :src="imgUrl"
     :srcset="imgSourceSet"
     class="hero-section"
-    sizes="(min-width: 600px) 100vw, 100vw, 100vw"
+    sizes="(min-width: 1264px) 1280px, 
+            (min-width: 960px) 960px,
+            (min-width:600px) 600px,  
+            100vw"
     alt="Naturheilpraxis Krause-Schäfer Deesem (Heilpraktikerin für Lohmar, Siegburg, Neunkirchen-Seelscheid)"
   ></v-img>
 </template>
